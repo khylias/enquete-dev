04992a033d0351c0ac722f9d8d2bfb630b2d17d4
Vincent Kraus <vkraus@sellsy.com>
2022-01-11 13:55:28 +0100

    Add layouts and VueAuthenticate

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,43 @@
 <template>
     <div>
-        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div class="text-center">
-                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                    <span class="block text-indigo-600">Les réponses au métier de développeur</span>
-                </h1>
-                <span class="text-center text-indigo-400 mt-2 font-semibold text-sm italic">(par des développeurs)</span>
-                <p class="mt-3 text-center text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-auto">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-                </p>
-            </div>
-            <section class="flex flex-wrap mx-auto my-8 mt-4">
-                <NuxtLink class="py-8 px-4 lg:w-1/3" 
-                    :to="`/questions/${question.slug}`"
-                    v-for="question of questions"
-                    :key="question.slug">
-                    <div class="h-full flex items-start">
-                        <div class="flex-grow pl-6">
-                            <h3 class="title-font text-xl font-medium text-gray-900 mb-3">{{ question.title }}</h3>
-                            <p class="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                            <a class="inline-flex items-center">
-                                <img
-                                    alt="blog"
-                                    src="https://dummyimage.com/103x103"
-                                    class="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
-                                >
-                                <span class="flex-grow flex flex-col pl-3">
-                                    <span class="title-font font-medium text-gray-900">Alper Kamu</span>
-                                </span>
-                            </a>
-                        </div>
+        <div class="text-center">
+            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span class="block text-indigo-600">Les réponses au métier de développeur</span>
+            </h1>
+            <span class="text-center text-indigo-400 mt-2 font-semibold text-sm italic">(par des développeurs)</span>
+            <p class="mt-3 text-center text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-auto">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+            </p>
+        </div>
+        <section class="flex flex-wrap mx-auto my-8 mt-24">
+            <NuxtLink
+                v-for="question of questions"
+                :key="question.slug"
+                :to="`/questions/${question.slug}`"
+                class="py-8 px-4 lg:w-1/3">
+                <div class="h-full flex items-start">
+                    <div class="flex-grow pl-6">
+                        <h3 class="title-font text-xl font-medium text-gray-900 mb-3">{{ question.title }}</h3>
+                        <p class="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a class="inline-flex items-center">
+                            <img
+                                alt="blog"
+                                src="https://dummyimage.com/103x103"
+                                class="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"
+                            >
+                            <span class="flex-grow flex flex-col pl-3">
+                                <span class="title-font font-medium text-gray-900">Alper Kamu</span>
+                            </span>
+                        </a>
                     </div>
-                </NuxtLink>
-            </section>
-        </main>
+                </div>
+            </NuxtLink>
+        </section>
     </div>
 </template>
 
 <script lang="ts">
-import FooterVue from '../components/Footer.vue'
-import HeaderVue from '../components/Header.vue'
-
 export default {
-    components: {
-        'header-main': HeaderVue,
-        'footer-main': FooterVue,
-    },
     async asyncData({ $content }: any) {
         const answers = await $content('answers')
             .only(['title', 'description', 'img', 'slug', 'author'])
--- pages/questions/_question.vue
+++ pages/questions/_question.vue
@@ -1,21 +1,48 @@
 <template>
   <div>
-    {{ question.title }}
+    <div class="text-center">
+      <h2
+        class="
+          text-4xl
+          tracking-tight
+          font-extrabold
+          text-gray-900
+          sm:text-5xl
+          md:text-6xl
+        "
+      >
+        <span class="block"> {{ question.title }}</span>
+      </h2>
+    </div>
+
+    <div class="grid grid-cols-3 gap-4 mt-24">
+        <div v-for="answer in answers" :key="answer.slug" class="border w-full bg-white dark:bg-gray-800 shadow-lg mx-auto rounded-xl p-4">
+            <nuxt-content :document="answer" class="text-gray-600 dark:text-white"/>
+            <div class="flex items-center mt-4">
+                <div class="flex flex-col ml-2 justify-between">
+                    <span class="font-semibold text-indigo-500 text-sm">
+                        Jean Miguel
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <p v-for="answer in answers" :key="answer.slug">
-        <nuxt-content :document="answer"/>
     </p>
   </div>
 </template>
 
 <script>
 export default {
+  layout: 'question',
   async asyncData({ $content, params }) {
-    const question = await $content('questions', params.question).fetch();
+    const question = await $content('questions', params.question).fetch()
     const answers = await $content('answers')
       .where({
-        'question': {
-          $eq: params.question
-        }
+        question: {
+          $eq: params.question,
+        },
       })
       .sortBy('createdAt', 'asc')
       .fetch()
@@ -25,9 +52,9 @@ export default {
     }
   },
   data() {
-      return {
-          question: {}
-      }
-  }
+    return {
+      question: {},
+    }
+  },
 }
 </script>
